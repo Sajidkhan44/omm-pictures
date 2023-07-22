8ef8dc2636a2b0bad61e60bcd581bbdac3cd325d
Sajidkhan44 <sajidkhan30301@gmail.com>
2023-07-22 21:14:45 +0530

    worked on prettier

--- src/components/pages/Birthdays.tsx
+++ src/components/pages/Birthdays.tsx
@@ -1,5 +1,5 @@
-import { babyshoot, maternity, work } from '@/libs/Data'
-import React from 'react'
+import { babyshoot, work } from "@/libs/Data";
+import React from "react";
 
 export default function Birthdays() {
   return (
@@ -30,22 +30,35 @@ export default function Birthdays() {
       </div>
       <div className="py-[100px]  space-y-12 lg:w-[90%] px-8 mx-auto ">
         <div className=" space-y-2 text-white  text-center">
-          <h1 className=" font-semibold md:text-5xl text-3xl uppercase">BABY</h1>
+          <h1 className=" font-semibold md:text-5xl text-3xl uppercase">
+            BABY
+          </h1>
         </div>
         <div className=" w-full grid lg:grid-cols-2 grid-cols-1 gap-6 md:py-12">
           <div className=" col-span-1  flex  justify-center">
             <div className=" space-y-4">
               <h1 className=" text-3xl font-medium text-white">
-              "Timeless Milestones: Capturing Birthday Joy and Baby's First Moments"
+                "Timeless Milestones: Capturing Birthday Joy and Baby's First
+                Moments"
               </h1>
               <p className=" text-md font-medium text-gray-300 text-justify">
-              Welcome to "Timeless Milestones," where we specialize in photography that beautifully captures the joy of birthdays and the precious moments of a baby's early days. From the exuberant celebrations of birthdays to the heartwarming magic of a baby's first smiles, our talented photographers are dedicated to preserving these treasured memories with creativity and care.
+                Welcome to "Timeless Milestones," where we specialize in
+                photography that beautifully captures the joy of birthdays and
+                the precious moments of a baby's early days. From the exuberant
+                celebrations of birthdays to the heartwarming magic of a baby's
+                first smiles, our talented photographers are dedicated to
+                preserving these treasured memories with creativity and care.
               </p>
               <p className=" text-md font-medium text-gray-300 text-justify">
                 <span className=" text-white text-xl inline-block">
-                Birthday Celebrations:
+                  Birthday Celebrations:
                 </span>{" "}
-                 "Timeless Milestones" brings to life the spirit of birthdays, capturing the happiness and excitement that surround these special occasions. Whether it's a 1st birthday, sweet sixteen, or a milestone year for an adult, our photography showcases the love shared with family and friends as the birthday person marks another year of life's journey.
+                "Timeless Milestones" brings to life the spirit of birthdays,
+                capturing the happiness and excitement that surround these
+                special occasions. Whether it's a 1st birthday, sweet sixteen,
+                or a milestone year for an adult, our photography showcases the
+                love shared with family and friends as the birthday person marks
+                another year of life's journey.
               </p>
               <p className=" text-md font-medium text-gray-300 text-justify">
                 <span className=" text-white text-xl inline-block">
@@ -58,32 +71,46 @@ export default function Birthdays() {
               </p>
               <p className=" text-md font-medium text-gray-300 text-justify">
                 <span className=" text-white text-xl inline-block">
-                A Joyful Narrative:
+                  A Joyful Narrative:
                 </span>{" "}
-                 Our photographers artfully document the essence of birthday parties, from the blowing out of candles to the contagious laughter and camaraderie. The images become a joyful narrative of these cherished moments, creating lasting keepsakes that families will treasure for generations.
-                Our photography style emphasizes elegance and timelessness,
-                ensuring that each portrait showcases the grace and beauty of
-                motherhood. We use creative lighting and artistic compositions
-                to craft images that will be treasured for generations to come.{" "}
+                Our photographers artfully document the essence of birthday
+                parties, from the blowing out of candles to the contagious
+                laughter and camaraderie. The images become a joyful narrative
+                of these cherished moments, creating lasting keepsakes that
+                families will treasure for generations. Our photography style
+                emphasizes elegance and timelessness, ensuring that each
+                portrait showcases the grace and beauty of motherhood. We use
+                creative lighting and artistic compositions to craft images that
+                will be treasured for generations to come.{" "}
               </p>
               <p className=" text-md font-medium text-gray-300 text-justify">
                 <span className=" text-white text-xl inline-block">
-                Baby's First Moments:
+                  Baby's First Moments:
                 </span>{" "}
-                 There is a unique magic in capturing a baby's first moments – the innocence, wonder, and unconditional love that radiate from the little bundle of joy. "Timeless Milestones" specializes in newborn and baby shoots, preserving the essence of these precious early days for families to cherish forever.
-                Maternity photography is not just about the expectant mother;
-                it's about celebrating the bond between mother and child. We
-                capture the tender moments of connection as the unborn baby is
-                lovingly embraced and cherished.
+                There is a unique magic in capturing a baby's first moments –
+                the innocence, wonder, and unconditional love that radiate from
+                the little bundle of joy. "Timeless Milestones" specializes in
+                newborn and baby shoots, preserving the essence of these
+                precious early days for families to cherish forever. Maternity
+                photography is not just about the expectant mother; it's about
+                celebrating the bond between mother and child. We capture the
+                tender moments of connection as the unborn baby is lovingly
+                embraced and cherished.
               </p>
               <p className=" text-md font-medium text-gray-300 text-justify">
                 <span className=" text-white text-xl inline-block">
-                Tender Connections:
+                  Tender Connections:
                 </span>{" "}
-                 Our photographers focus on the tender connections between parents and their newborns, capturing the purest expressions of love and devotion. The gentle touch, the loving gaze, and the warmth of family bonds form a heartwarming tableau of love in our baby photoshoots.
-                Personalized and Meaningful: Every maternity photography session at "Radiant Expectations" is tailored to the unique personality and preferences of the expectant mother. We collaborate closely with our clients to create photographs that hold deep emotional significance.
+                Our photographers focus on the tender connections between
+                parents and their newborns, capturing the purest expressions of
+                love and devotion. The gentle touch, the loving gaze, and the
+                warmth of family bonds form a heartwarming tableau of love in
+                our baby photoshoots. Personalized and Meaningful: Every
+                maternity photography session at "Radiant Expectations" is
+                tailored to the unique personality and preferences of the
+                expectant mother. We collaborate closely with our clients to
+                create photographs that hold deep emotional significance.
               </p>
-              
             </div>
           </div>
           <div className="max-w-full max-h-full col-span-1 relative rounded-[20px] overflow-hidden">
@@ -107,5 +134,5 @@ export default function Birthdays() {
         </div>
       </div>
     </div>
-  )
+  );
 }
