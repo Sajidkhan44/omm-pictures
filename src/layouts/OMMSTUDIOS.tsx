23b489a8b67ab9bf1cba27e6059aaca7333e351c
Sajidkhan44 <sajidkhan30301@gmail.com>
2023-07-23 02:09:03 +0530

    cleared ommstudeios

--- src/layouts/OMMSTUDIOS.tsx
+++ src/layouts/OMMSTUDIOS.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
 import Head from './Head';
-export interface props {
-  children?: React.ReactNode; 
+
+export interface OMMSTUDIOSProps {
+  children?: React.ReactNode;
+  seo?: {
+    pageTitle: string;
+    description: string;
+    url: string;
+    // Add more properties if needed
+  };
 }
-export default function OMMSTUDIOS({children, seo}:props) {
+
+export default function OMMSTUDIOS({ children, seo }: OMMSTUDIOSProps) {
   return (
     <div className="">
-      {
-        seo && <Head pageTitle={seo.pageTitle} description={seo.description} url={seo.url}/>
-      }
-        <Navbar />
-        {children}
-        <Footer />
+      {seo && <Head pageTitle={seo.pageTitle} description={seo.description} url={seo.url} />}
+      <Navbar />
+      {children}
+      <Footer />
     </div>
-  )
+  );
 }
